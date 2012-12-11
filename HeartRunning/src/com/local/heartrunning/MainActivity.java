4494c1d3dbcad26073801ad97e683a747787cb73
Joshua <josh.c.2009@my.bristol.ac.uk>
2012-12-11 22:39:44 +0000

    Facebook login screen pops up on application start (you can login, but nothing happens at the moment)

--- HeartRunning/src/com/local/heartrunning/MainActivity.java
+++ HeartRunning/src/com/local/heartrunning/MainActivity.java
@@ -1,14 +1,19 @@
 package com.local.heartrunning;
 
 import android.os.Bundle;
-import android.app.Activity;
+import com.facebook.FacebookActivity;
+import com.facebook.Request;
+import com.facebook.Response;
+import com.facebook.SessionState;
+import com.facebook.model.GraphUser;
+
 import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FacebookActivity {
 
 	// GUI Components
 	Button loginButton;
@@ -26,12 +31,34 @@ public class MainActivity extends Activity {
 				loadMenu();			
 			}
 		});
+        this.openSession();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
+    }
+    
+    @Override
+    protected void onSessionStateChange(SessionState state, Exception e) {
+    	//TODO: this is a copy paste from facebook
+    	// user has either logged in or not ...
+		  if (state.isOpened()) {
+		    // make request to the /me API
+		    Request request = Request.newMeRequest(this.getSession(), new Request.GraphUserCallback() {
+		        // callback after Graph API response with user object
+		        @Override
+		        public void onCompleted(GraphUser user, Response response) {
+		          if (user != null) {
+	//    	            TextView welcome = (TextView) findViewById(R.id.welcome);
+	//    	            welcome.setText("Hello " + user.getName() + "!");
+		          }
+		        }
+		      }
+		    );
+		    Request.executeBatchAsync(request);
+		  }
     }
     
     public void loadMenu() {
